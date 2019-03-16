30af9a9ac51fcf0dc79620a90b4744e4d37f1748
ekoly <cowley.erik@gmail.com>
2019-03-15 21:25:48 -0500

    added package

---
+++ js/app.js
@@ -0,0 +1,45 @@
+(()=>{
+
+	stromsy.app = angular.module('StromsyApp', ["ngRoute"]);
+
+    stromsy.app.config(($routeProvider)=>{
+        $routeProvider
+            .when('/',
+                    {
+                        templateUrl: 'partials/home-template.html',
+                        controller: 'homeCtrl',
+                        activetab: 'home'
+                    })
+            .when('/about',
+                    {
+                        templateUrl: 'partials/about-template.html',
+                        activetab: 'about'
+
+                    })
+            .when('/links',
+                    {
+                        templateUrl: 'partials/links-template.html',
+                        activetab: 'links'
+
+                    })
+            .when('/projects',
+                    {
+                        templateUrl: 'partials/projects-template.html',
+                        activetab: 'projects'
+
+                    })
+            .when('/login',
+                    {
+                        templateUrl: 'partials/login-template.html',
+                        activetab: 'login'
+
+                    })
+            .otherwise({redirectTo: '/'});
+    });
+
+    stromsy.app.controller("mainCtrl", function($scope, $route) {
+        $scope.route = $route;
+    });
+
+    
+})();
---
+++ js/base.js
@@ -0,0 +1,6 @@
+const stromsy = {
+    random: {
+        choice: (arr) => arr[Math.floor(Math.random()*arr.length)],
+        integer: (ceil) => Math.floor(Math.random()*ceil)
+    }
+};
